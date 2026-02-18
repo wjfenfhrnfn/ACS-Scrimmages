b66453ac720e8b010e13452b0027094958590dfb
NITI <96678122+wjfenfhrnfn@users.noreply.github.com>
2026-02-19 05:35:53 +0900

    Create deploy-arcane-commands.js

---
+++ deploy-arcane-commands.js
@@ -0,0 +1,45 @@
+import { REST, Routes } from 'discord.js';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath, pathToFileURL } from 'node:url';
+import 'dotenv/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const commands = [];
+const foldersPath = path.join(__dirname, 'SlashCommands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = await import(pathToFileURL(filePath));
+        
+    if (command.data && command.execute) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(`[경고] ${filePath} 에 'data' 또는 'execute' 속성이 없습니다.`);
+    }
+  }
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log(`${commands.length}개의 슬래시 커맨드 배포를 시작합니다...`);
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.ARCANE_ID),
+        { body: commands },
+    );
+
+    console.log(`성공적으로 ${data.length}개의 커맨드를 등록했습니다!`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
